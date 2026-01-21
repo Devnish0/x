aad21cc4accb87c2b8d384f0b53b1c03d2d31543
devnish <156004996+Devnish0@users.noreply.github.com>
2026-01-21 16:01:59 +0530

    deleted the unused date formatter

--- frontend/src/components/Mainpost.jsx
+++ frontend/src/components/Mainpost.jsx
@@ -21,18 +21,6 @@ const Mainpost = ({
   const { name, username } = input?.user;
   const [likeCount, setlikeCount] = useState(likes.length ?? 0);
 
-  //   const timeAgoShort = (date) => {
-  //     const seconds = Math.floor((Date.now() - new Date(date)) / 1000);
-  //     if (seconds < 60) return "now";
-  //     const minutes = Math.floor(seconds / 60);
-  //     if (minutes < 60) return `${minutes}m`;
-  //     const hours = Math.floor(minutes / 60);
-  //     if (hours < 24) return `${hours}h`;
-  //     const days = Math.floor(hours / 24);
-  //     if (days < 7) return `${days}d`;
-  //     const weeks = Math.floor(days / 7);
-  //     return `${weeks}w`;
-  //   };
   const formattedTime = (createdAt) => {
     const formatted = new Date(createdAt)
       .toLocaleString("en-IN", {
